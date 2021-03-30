331b01967877f7df2dd7fd945830a53d255318a6
Usama Baig <macbookpro@usama-baig.home>
2021-03-30 12:58:29 +0500

    Successfully added put get post patch and delete api views in this project

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 
 class HelloAPIView(APIView):
     """TEST API VIEW"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """ Returns a list of APIView Features"""
@@ -14,3 +17,30 @@ class HelloAPIView(APIView):
         'Is mapped manually to URLS',
         ]
         return Response({'message' : "Hello",'an_APIView' : an_APIView})
+
+    def post(self, request):
+        """Create a hello message with our name """
+        serializer = self.serializer_class(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'hello {name}'
+            return Response({'message' : message})
+        else:
+            return Response(
+            serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """Handle Updating an Object"""
+        return Response({'method' : 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle Partial Update of Object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self,request,pk=None):
+        """Deletes an Object"""
+        return Response({'method':'delete'})
+        
