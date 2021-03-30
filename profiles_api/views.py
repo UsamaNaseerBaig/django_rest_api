a1697453ad4c14935635432658bcfb38fa5afbfe
Usama Baig <macbookpro@usama-baig.home>
2021-03-30 20:17:48 +0500

    successfully added permission to rest framework

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
 from rest_framework import viewsets
+from profiles_api import models
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
 
 
 class HelloAPIView(APIView):
@@ -88,3 +91,11 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request,pk=None):
         """Handle Removing an Object"""
         return Response({'http_method':'Delete'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permissions_classes = (permissions.UpdateOwnProfile,)
